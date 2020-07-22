c52ca71fba8881807cf2afa5d514d22ce6028178
mauricioolarte <reneolarte@yahoo.com>
2020-07-22 15:06:48 +0000

    correct parent of left child

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -32,6 +32,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		if (new_node == NULL)
 			return (NULL);
 		new_node->left = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+		if (new_node->left == NULL)
+			return (NULL);
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
 		parent->left = new_node;
