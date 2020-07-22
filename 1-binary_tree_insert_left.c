9661538d1f3102b1bbd66181702c68f0453c8a32
mauricioolarte <reneolarte@yahoo.com>
2020-07-22 14:50:11 +0000

    correct parent of left child

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,7 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		if (new_node == NULL)
 			return (NULL);
 		parent->left = new_node;
-		new_node->parent = parent->left;
+		new_node->parent = parent;
 		new_node->n = value;
 		new_node->left = NULL;
 		new_node->right = NULL;
